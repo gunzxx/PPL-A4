80036e0f8eae16f7eb95768359c4c23fa13e4210
gunzxx <ggm5850@gmail.com>
2023-04-29 21:43:10 +0700

    change delete message

--- resources/views/partners/pengelola/agreements/index.blade.php
+++ resources/views/partners/pengelola/agreements/index.blade.php
@@ -85,7 +85,7 @@
 
     <div class="popup-backdrop delete-agreement-popup">
         <div class="popup-container">
-            <div class="popup-text">Hapus persetujuan?</div>
+            <div class="popup-text">Apakah yakin melakaukan menghapus data?</div>
             <div class="popup-alert">
                 <button onclick="deleteAgreement()" value="true" class="popup-confirm popup-yes" type="button">Yes</button>
                 <button value="false" class="popup-confirm popup-no" type="button">No</button>
