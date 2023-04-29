a579020681df867e74ef03eb26ed0e40506050b5
gunzxx <ggm5850@gmail.com>
2023-04-29 19:15:03 +0700

    change button "batalkan" to "hapus"

--- resources/views/partners/pengelola/offers/index.blade.php
+++ resources/views/partners/pengelola/offers/index.blade.php
@@ -61,10 +61,7 @@
                             <button data-detail-id="{{ $detail->id }}" data-offer-id="{{ $detail->offer->id }}" class="btn reject" type="button">Reject<i class="bi bi-x-lg"></i></button>
                         @elseif($detail->is_approved == 1)
                             <span class="status is_confirm">Diterima <i class="bi bi-check-circle-fill"></i></span>
-                            <button data-detail-id="{{ $detail->id }}" data-offer-id="{{ $detail->offer->id }}" class="btn cancel" type="button">Batalkan<i class="bi bi-x-lg"></i></button>
-                        @elseif($detail->is_rejected == 1)
-                            <span class="status is_reject">Ditolak </span>
-                            <button data-detail-id="{{ $detail->id }}" data-offer-id="{{ $detail->offer->id }}" class="btn cancel" type="button">Hapus<i class="bi bi-trash3-fill"></i></button>
+                            <button data-detail-id="{{ $detail->id }}" data-offer-id="{{ $detail->offer->id }}" class="btn cancel" type="button">Hapus<i class="bi bi-x-lg"></i></button>x
                         @endif
                     </div>
                 </div>
