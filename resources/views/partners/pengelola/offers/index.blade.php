fafc4cb4cfb4e7b2525ca67af3310b401789468e
gunzxx <ggm5850@gmail.com>
2023-05-01 20:59:29 +0700

    change update agreements

--- resources/views/partners/pengelola/offers/index.blade.php
+++ resources/views/partners/pengelola/offers/index.blade.php
@@ -49,8 +49,8 @@
                     </div>
                     <div class="card-action">
                         @if ($detail->is_approved == 0 && $detail->is_rejected == 0)
-                            <button data-detail-id="{{ $detail->id }}" data-offer-id="{{ $detail->offer->id }}" class="btn confirm" type="button">Terima<i class="bi bi-check-lg"></i></button>
-                            <button data-detail-id="{{ $detail->id }}" data-offer-id="{{ $detail->offer->id }}" class="btn reject" type="button">Tolak<i class="bi bi-x-lg"></i></button>
+                            <button data-detail-id="{{ $detail->id }}" data-offer-id="{{ $detail->offer->id }}" class="btn confirm" type="button">Accept<i class="bi bi-check-lg"></i></button>
+                            <button data-detail-id="{{ $detail->id }}" data-offer-id="{{ $detail->offer->id }}" class="btn reject" type="button">Reject<i class="bi bi-x-lg"></i></button>
                         @elseif($detail->is_approved == 1)
                             <span class="status is_confirm">Diterima <i class="bi bi-check-circle-fill"></i></span>
                             <button data-detail-id="{{ $detail->id }}" data-offer-id="{{ $detail->offer->id }}" class="btn cancel" type="button">Hapus<i class="bi bi-x-lg"></i></button>
