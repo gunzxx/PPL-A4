216caec906030baef7b154603cb57e9628fc10b2
gunzxx <ggm5850@gmail.com>
2023-05-03 14:30:36 +0700

    update alert style

--- resources/views/components/alertSuccess.blade.php
+++ resources/views/components/alertSuccess.blade.php
@@ -1,6 +1,6 @@
 <div class="alert-container alert-container-success">
     <div class="alert-body">
-        <i class="bi bi-exclamation-triangle-fill"></i><span>{{ $message }}</span>
+        <i class="bi bi-shield-fill-check"></i><p>{{ $message }}</p>
     </div>
     <button type="button" class="alert-x">&#10005;</button>
     <div class="alert-loader"></div>
