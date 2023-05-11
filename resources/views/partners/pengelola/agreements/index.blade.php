32110c56d480f2f4daa6f943694e2a2e4ddadf58
gunzxx <ggm5850@gmail.com>
2023-05-11 21:26:11 +0700

    add delete button on pengelola agreement

--- resources/views/partners/pengelola/agreements/index.blade.php
+++ resources/views/partners/pengelola/agreements/index.blade.php
@@ -63,9 +63,11 @@
                         @if ($agreement_detail->status == "waiting")
                             <a class="btn" href="/pengelola/partners/agreements/edit/{{ $agreement_detail->id }}">Update <i class="bi bi-pencil-square"></i></a>
                             <button class="btn delete delete-agreement" data-agreement-id="{{ $agreement_detail->agreement->id }}" data-agrement-detail-id="{{ $agreement_detail->id }}" type="button">Hapus <i class="bi bi-trash-fill"></i></button>
-                        @elseif($agreement_detail->status == "accept")
+                            @elseif($agreement_detail->status == "accept")
                             {{-- <span class="status is_confirm">Diterima <i class="bi bi-check-circle"></i></span> --}}
                             <button data-agrement-detail-id="{{ $agreement_detail->id }}" data-agreement-id="{{ $agreement_detail->agreement->id }}" class="btn cancel delete-agreement" type="button">Hapus<i class="bi bi-x-lg"></i></button>
+                            @elseif($agreement_detail->status == "reject")
+                            <button class="btn delete delete-agreement" data-agreement-id="{{ $agreement_detail->agreement->id }}" data-agrement-detail-id="{{ $agreement_detail->id }}" type="button">Hapus <i class="bi bi-trash-fill"></i></button>
                         @endif
                     </div>
                 </div>
