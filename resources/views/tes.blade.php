f5fe949e6a663a73f84ed43e72e7320bf8fdf37b
gunzxx <ggm5850@gmail.com>
2023-05-14 19:20:58 +0700

    create custom alert

--- resources/views/tes.blade.php
+++ resources/views/tes.blade.php
@@ -1,4 +1,8 @@
-<form action="/tes-media" method="post" enctype="multipart/form-data">
+@extends('template.main')
+
+{{-- Tes media --}}
+
+{{-- <form action="/tes-media" method="post" enctype="multipart/form-data">
     <img height="100" src="{{ $user->getFirstMediaUrl('tes') }}" alt="">
     @csrf
     <input name="image" required accept="image/jpg, image/png, image/jpeg" type="file">
@@ -6,4 +10,19 @@
         {{ $message }}
     @enderror
     <button type="submit">Kirim</button>
-</form>
+</form> --}}
+
+
+{{-- Tes alert --}}
+@section('content')
+    <div class="flex">
+        <button id="klik">Klik bang</button>
+    </div>
+    <script>
+        $("#klik").click(
+            function(){
+                Swal.fire('Dek iyeh bang?');
+            }
+        )
+    </script>
+@endsection
