c049190f4212ec98c12a9e0fa8dbb00537ff8bdd
gunzxx <ggm5850@gmail.com>
2023-06-03 10:04:17 +0700

    update max digit no_rek

--- app/Http/Controllers/PetaniShopController.php
+++ app/Http/Controllers/PetaniShopController.php
@@ -56,7 +56,7 @@ class PetaniShopController extends Controller
             'bean_type'=>'required',
             'stok'=>'required|numeric',
             'price'=>'required|numeric',
-            'no_rek'=>'required|numeric',
+            'no_rek'=>'required|numeric|max:16',
             'agreement_detail_id'=>'required',
         ]);
         $validated['petani_id']=auth()->user()->id;
