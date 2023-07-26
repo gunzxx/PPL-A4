74b4121c1435b4a13057e9d8c38fa73b0e6ba9fb
gunzxx <ggm5850@gmail.com>
2023-07-26 17:34:50 +0700

    add meta dicoding

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="dicoding:email" content="wwwnuriscs76@gmail.com">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ isset($title) ? ucwords($title)." - " : "" }}SoySync</title>
